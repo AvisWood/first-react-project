71cc8ec3623fc620ad424a053675050c1d925794
COR Students <corstudents@Matts-MacBook-Pro-22.local>
2019-03-18 11:39:42 -0400

    react profile

---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class Header extends Component{
+    render(){
+        return(
+        <div className="intro">
+            <h2>AVIS WOODEN</h2>
+            <h3>Software Developer</h3>
+            <h3>Some Witty summary will go here.</h3>
+           </div>
+  )
+}
+       
+    }
+
+
+export default Header;
---
+++ src/Components/Skills.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class Skills extends Component{
+    render(){
+        return(
+            <div className="Skills">
+                <div className="skillsRow">
+                    <ul>
+                        <li>Microsoft Office</li>
+                        <li>Python</li>
+                        <li>HTML</li>
+                        <li>Cascading Style Sheets (CSS)</li>
+                    <li>JavaScript</li>
+                    <li>jQuery</li>
+                    <li>Bootstrap</li>
+                    <li>Node.js</li>
+                    <li>MongoDB</li>
+                    <li>React.js</li>
+                    <li>NoSQL</li>
+                    <li>Leadership</li>
+                    <li>Problem Solving</li>
+                    <li>Teamwork</li>
+                    </ul>
+          </div>
+         </div>
+        );
+    }
+}
+
+export default Skills;
